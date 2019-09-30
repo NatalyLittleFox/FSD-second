1191513221c01ec7f0a07a0be5a15ab1906ee3fc
Alexey Gmitron <nortimgg@gmail.com>
2019-09-30 20:48:58 +0700

    Add range-slider

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,7 @@ import * as dropdown from './dropdown/dropdown.js';
 import * as checkbox from './checkbox/checkbox.js';
 import * as likeButton from './like-button/like-button.js'
 import * as rateButton from './rate-button/rate-button.js'
+import * as rangeSlider from './range-slider/range-slider.js'
 
 
 global.jquery = global.jQuery = global.$ = $;
@@ -17,3 +18,5 @@ checkbox.checkboxListShow();
 likeButton.like()
 
 rateButton.rate();
+
+rangeSlider.slider();
